869cb3f61c4e7af5ed647ff10ce7f3be5a9f4bbb
AbhilashReddyPatlolla <abhilashreddypatel@outlook.com>
2025-02-28 14:35:25 +0530

    creation of components

--- src/app/pages/Admin/login/login.component.ts
+++ src/app/pages/Admin/login/login.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
